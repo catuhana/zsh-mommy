c56bbea497d063cb31535a6b8c0759b9141bc95d
tuhana <git@tuhana.me>
2023-06-27 01:42:21 +0300

    impwuvments :3

--- zsh-mommy.zsh
+++ zsh-mommy.zsh
@@ -2,75 +2,75 @@
 
 autoload -Uz add-zsh-hook
 
-ZSH_MOMMY_RESPONSE_TYPES=(POSITIVE NEGATIVE)
+readonly ZSH_MOMMY_RESPONSE_TYPES=(POSITIVE NEGATIVE)
 ZSH_MOMMY_POSITIVE_RESPONSES=(
   '*pets your head*'
-  'you'\''re such a smart cookie~ ❤️'
-  'that'\''s a good {AFFECTIONATE_TERM}~ ❤️'
+  $'you\'re such a smart cookie~ ❤️'
+  $'that\'s a good {AFFECTIONATE_TERM}~ ❤️'
   '{MOMMYS_ROLE} thinks {MOMMYS_PRONOUN} litle {AFFECTIONATE_TERM} earned a big hug~ ❤️'
-  'good {AFFECTIONATE_TERM}~\n{MOMMYS_ROLE}'\''s so proud of you~ ❤️'
+  $'good {AFFECTIONATE_TERM}~\n{MOMMYS_ROLE}\'s so proud of you~ ❤️'
   'awe, what a good {AFFECTIONATE_TERM}~\n{MOMMYS_ROLE} knew you could do it~ ❤️'
-  'that'\''s a good {AFFECTIONATE_TERM}~ ❤️'
+  $'that\'s a good {AFFECTIONATE_TERM}~ ❤️'
   '{MOMMYS_ROLE} is so proud of {MOMMYS_PRONOUN} {AFFECTIONATE_TERM}~ ❤️'
 )
 ZSH_MOMMY_NEGATIVE_RESPONSES=(
   '{MOMMYS_ROLE} believes in you~ ❤️'
-  'do you need {MOMMYS_ROLE}'\''s help~? ❤️'
+  $'do you need {MOMMYS_ROLE}\'s help~? ❤️'
   '{MOMMYS_ROLE} still loves you no matter what~ ❤️'
-  'oh no did {MOMMYS_ROLE}'\''s little {AFFECTIONATE_TERM} made a big mess~? ❤️'
+  $'oh no did {MOMMYS_ROLE}\'s little {AFFECTIONATE_TERM} made a big mess~? ❤️'
   '{MOMMYS_ROLE} knows {MOMMYS_PRONOUN} little {AFFECTIONATE_TERM} can do better~ ❤️'
   'just a little further, sweetie~ ❤️'
   '{MOMMYS_ROLE} believes in you~ ❤️'
-  'let'\''s try again together my {AFFECTIONATE_TERM}~ ❤️'
+  $'let\'s try again together my {AFFECTIONATE_TERM}~ ❤️'
   '{MOMMYS_ROLE} is here to help you through it~ ❤️'
 )
 
-ZSH_MOMMY_AFFECTIONATE_TERM_PLACEHOLDER='{AFFECTIONATE_TERM}'
-ZSH_MOMMY_MOMMYS_PRONOUN_PLACEHOLDER='{MOMMYS_PRONOUN}'
-ZSH_MOMMY_MOMMYS_ROLE_PLACEHOLDER='{MOMMYS_ROLE}'
+readonly ZSH_MOMMY_AFFECTIONATE_TERM_PLACEHOLDER='{AFFECTIONATE_TERM}'
+readonly ZSH_MOMMY_MOMMYS_PRONOUN_PLACEHOLDER='{MOMMYS_PRONOUN}'
+readonly ZSH_MOMMY_MOMMYS_ROLE_PLACEHOLDER='{MOMMYS_ROLE}'
 
-ZSH_MOMMY_AFFECTIONATE_TERM=(girl)
-ZSH_MOMMY_MOMMYS_PRONOUN=(her)
-ZSH_MOMMY_MOMMYS_ROLE=(mommy)
+ZSH_MOMMY_AFFECTIONATE_TERM=${ZSH_MOMMY_AFFECTIONATE_TERM:-(girl)}
+ZSH_MOMMY_MOMMYS_PRONOUN=${ZSH_MOMMY_MOMMYS_PRONOUN:-(her)}
+ZSH_MOMMY_MOMMYS_ROLE=${ZSH_MOMMY_MOMMYS_ROLE:-(mommy)}
 
-# ZSH_MOMMY_RUN_AFTER_EVERY_COMMAND=false
+ZSH_MOMMY_RUN_AFTER_EVERY_COMMAND=${ZSH_MOMMY_RUN_AFTER_EVERY_COMMAND:-true}
 
-if [[ ! $ZSH_MOMMY_RUN_AFTER_EVERY_COMMAND ]]; then
+if [[ $ZSH_MOMMY_RUN_AFTER_EVERY_COMMAND == true || $ZSH_MOMMY_RUN_AFTER_EVERY_COMMAND -ge 1 ]] {
   add-zsh-hook precmd _mommy
-else
+} else {
   add-zsh-hook -d precmd _mommy
 
-  mommy() {
-    eval "$@" ; _mommy $?
+  function mommy {
+    "$@" ; _mommy $?
   }
-fi
+}
 
-_mommy() {
+function _mommy {
   local command_status=${1:-$?}
   local response_type_index=$((command_status == 0 ? 1 : 2))
   print $(create_response ${ZSH_MOMMY_RESPONSE_TYPES[$response_type_index]})
 }
 
-precmd() {
+function precmd {
   : $RANDOM
 }
 
-create_response() {
+function create_response {
   local random_affectionate_term=$(pick_random_value $ZSH_MOMMY_AFFECTIONATE_TERM)
   local random_pronoun=$(pick_random_value $ZSH_MOMMY_MOMMYS_PRONOUN)
   local random_role=$(pick_random_value $ZSH_MOMMY_MOMMYS_ROLE)
 
-  if [[ ${@[1]} == ${ZSH_MOMMY_RESPONSE_TYPES[1]} ]]; then
+  if [[ $1 == ${ZSH_MOMMY_RESPONSE_TYPES[1]} ]] {
     local random_response=$(pick_random_value $ZSH_MOMMY_POSITIVE_RESPONSES)
-  else
+  } else {
     local random_response=$(pick_random_value $ZSH_MOMMY_NEGATIVE_RESPONSES)
-  fi
+  }
   random_response=$(replace_placeholders $random_response $random_affectionate_term $random_pronoun $random_role)
 
   print "$random_response"
 }
 
-replace_placeholders() {
+function replace_placeholders {
   local response=${@[1]}
   response=${response//$ZSH_MOMMY_AFFECTIONATE_TERM_PLACEHOLDER/${@[2]}}
   response=${response//$ZSH_MOMMY_MOMMYS_PRONOUN_PLACEHOLDER/${@[3]}}
@@ -78,7 +78,7 @@ replace_placeholders() {
   print "$response"
 }
 
-pick_random_value() {
+function pick_random_value {
   local values=($@)
   local random_index=$((RANDOM % ${#values[@]} + 1))
   print ${values[random_index]}
